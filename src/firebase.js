07656efcd748697ce38e850e60f77d336301606d
Dignity Dev <bashirhaadiy@gmail.com>
2022-02-11 19:22:49 +0100

    init

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyDXBgKN1BhKDsv9fwghgpOBHg4JubrQDwQ",
+    authDomain: "clone-32daa.firebaseapp.com",
+    projectId: "clone-32daa",
+    storageBucket: "clone-32daa.appspot.com",
+    messagingSenderId: "495862591037",
+    appId: "1:495862591037:web:e77a8ab846540fbffd8454",
+    measurementId: "G-YNGVCE4G8E"
+};
